57b469873ea82fa98fe3dd5838fae116926f7d2e
Uliwazeer <ali.wazeer2000@gmail.com>
2025-07-25 00:21:26 +0300

    Test

--- controllers/completedtaskController.js
+++ controllers/completedtaskController.js
@@ -3,20 +3,33 @@ const Dashboard = require('../models/dashboard');
 const User = require('../models/register');
 
 module.exports.completedtask = function(req, res){
-    const data = Dashboard.find({})
+    Dashboard.find({})
     .then(function(data){
-        User.findOne({email : "ankitvis609@gmail.com"})
+        User.findOne({ email: "ankitvis609@gmail.com" })
         .then(function(user){
+            if (!user) {
+                console.log('User not found');
+                return res.render('completedtask', {
+                    title: "Dashboard",
+                    name: "Guest",
+                    dashboard: data
+                });
+            }
+
             console.log(`**********user`, user.name);
-        return res.render('completedtask', {
-            title: "Dashboard",
-            name: user.name,
-            dashboard: data
+            return res.render('completedtask', {
+                title: "Dashboard",
+                name: user.name,
+                dashboard: data
+            });
+        })
+        .catch(function(err){
+            console.log('Error finding user', err);
+            return res.status(500).send('Server Error');
         });
     })
-    })
     .catch(function(err){
-        console.log('Error', err);
-        return;
+        console.log('Error finding dashboard data', err);
+        return res.status(500).send('Server Error');
     });
-}
+};
